5725f7fcc8867dbc82eca1f4216ba2c66c13ef3f
wildanibr <silentsoul4693@gmail.com>
2023-05-30 21:45:13 +0700

    navbar

---
+++ src/scripts/views/pages/about-us.js
@@ -0,0 +1,14 @@
+const About = {
+    async render() {
+      return `
+        <h2>Halaman Favorit SIDGI</h2>
+        <h3>memuat data diri tim capstone</h3>
+      `;
+    },
+   
+    async afterRender() {
+      // Fungsi ini akan dipanggil setelah render()
+    },
+  };
+   
+  export default About;
---
+++ src/scripts/views/pages/main.js
@@ -0,0 +1,14 @@
+const Main = {
+  async render() {
+    return `
+      <h2>Halaman Utama SIDGI</h2>
+      <h3>memuat tampilan utama dan fitur"</h3>
+    `;
+  },
+ 
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+ 
+export default Main;
