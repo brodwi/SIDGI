628e3bbfcd53263712b2de004f328a5a9d7f3ff7
Fadhilah Ismarani <fadhilahismarani2@gmail.com>
2023-06-13 21:43:35 +0700

    jumbroton

--- src/scripts/views/pages/landing-page.js
+++ src/scripts/views/pages/landing-page.js
@@ -4,7 +4,7 @@ const LandingPage = {
         <div class="hero" style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('images/hero1.jpeg');">
     <div class="heroinner">
         <h1 class="hero-title">SIDGI</h1>
-        <p class="hero-desc">Sistem Informasi Data Gunung di indonesia.</p>
+        <p class="hero-desc">SIDGI (Sistem Informasi Data Gunung di Indonesia) merupakan ensiklopedia mengenai data gunung di Indonesia. Sistem ini menyajikan beberapa informasi seperti sejarah gunung, lokasi gunung, hingga status gunung.</p>
         
         <button onclick="window.location.href='#/main'" class="btn">START</button>
     </div>
