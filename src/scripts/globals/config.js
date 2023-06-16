d7f8599ba23aef34d9dd237bf8c63813d8724078
Anggaranii <anggarani72@gmail.com>
2023-06-16 16:45:20 +0700

    favorite

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,9 @@
 const CONFIG = {
-    BASE_URL: 'https://api-gunung.vercel.app/'
-  };
+  BASE_URL: 'https://api-gunung.vercel.app/',
+  DATABASE_NAME: 'gunung-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'gunungs',
+};
   
-  export default CONFIG;
+export default CONFIG;
   
--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,20 +1,35 @@
 import UrlParser from '../../routes/url-parser';
 import gunungResource from '../../data/gunung-resource';
 import { createGunungDetailTemplate } from '../templates/templates-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 
 const Detail = {
   async render() {
     return `
-          <div id="detailGunung" class="detailGunung"></div>
-          `;
+      <div id="detailGunung" class="detailGunung"></div>
+      <div id="likeButtonContainer"></div>
+    `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const gunung = await gunungResource.detailGunung(url.id);
     const detailgunung = gunung.data.note;
-    const restaurantContainer = document.querySelector('#detailGunung');
-    restaurantContainer.innerHTML = createGunungDetailTemplate(detailgunung);
+    const gunungContainer = document.querySelector('#detailGunung');
+    gunungContainer.innerHTML = createGunungDetailTemplate(detailgunung);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      gunung: {
+        nama: gunung.nama,
+        bentuk: gunung.bentuk,
+        tinggi: gunung.tinggi,
+        letusan_terakhir: gunung.letusan_terakhir,
+        geolokasi: gunung.geolokasi,
+        status: gunung.status,
+        keamanan: gunung.keamanan,
+      },
+    });
   },
 };
 
